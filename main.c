dc8dbdcea3693b75e5c0c7eb4f14108cb027fae1
Rohan Carty <rohan253@hotmail.com>
2022-02-24 22:26:00 +1100

    Added super rudimentary input handling
    successfully sets up a callback to process the controller during vblank and increments/decrements the counter

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <yaul.h>
 
+// Void function pointers for callbacks/interrupts
+static void _vblank_out_handler(void *);
+
+static smpc_peripheral_digital_t g_k_digitalPad;
+
+
 void user_init(void)
 {
         vdp2_tvmd_display_res_set(VDP2_TVMD_INTERLACE_NONE,
@@ -11,6 +17,8 @@ void user_init(void)
 
         dbgio_dev_default_init(DBGIO_DEV_VDP2_ASYNC); // Init debug output using the VDP2 driver
         dbgio_dev_font_load(); // Load the font for dbgio
+
+        vdp_sync_vblank_out_set(_vblank_out_handler, NULL); // Assign a callback function for vblank, mostly for collecting peripheral information
 
         vdp2_tvmd_display_set(); // Enable display output from VDP2
 
@@ -29,11 +37,34 @@ int main(void)
                 dbgio_printf("\x1b[1;1H\x1b[2JHello, World!\nCounter: %i\n", iDx); // Write text to debug text buffer
                 dbgio_flush(); // Flushes the dbgio buffer to draw to the selected driver 
 
-                iDx++;
+                // Test to see if controller buttons have been pushed
+                smpc_peripheral_process(); // Process all inputs
+                smpc_peripheral_digital_port(1, &g_k_digitalPad); // Assign processed inputs to peripheral device? No documentation to confirm
+
+                if (g_k_digitalPad.held.button.start != 0) // If start button pressed
+                {
+                        smpc_smc_sysres_call(); // Reset the whole console
+
+                        return 0;
+                }
+
+                if ((g_k_digitalPad.pressed.raw & PERIPHERAL_DIGITAL_UP) != 0)    // If D-Pad up has been pressed
+                {
+                        iDx++;                                                  // Increent the counter
+                } 
+                else if ((g_k_digitalPad.pressed.raw & PERIPHERAL_DIGITAL_DOWN) != 0) // Otherwise if D-Pad down has been pressed
+                {
+                        iDx--;                                                  // Decrement the counter
+                }
 
                 vdp2_sync(); // Synchronises the buffered commands waiting for VDP2
                 vdp2_sync_wait(); // Wait for all synchronise commands to finish as they're non-blocking
                 // returns back to start of VBLANK as well
         }
         return 0;
+}
+
+static void _vblank_out_handler(void *work __unused)
+{
+        smpc_peripheral_intback_issue();
 }
